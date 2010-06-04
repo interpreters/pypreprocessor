6951ce5d95f826cd0e1355b3cad423a5de5edc1e
Evan Plaice <evanplaice@gmail.com>
2010-06-04 14:23:50 -0600

    Updated the py2and3 example
    py2and3.py
    * simplified the code to be shorter and more concise
    * added the #exclude and #unexclude directive to squelch the preprocessor setup code
    * added a docstring block description for users
    * cleaned whitespace

--- Examples/py2and3.py
+++ Examples/py2and3.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 # py2and3.py
 
+'''
+py2and3.py - Python2x and Python3x playing nicely together.
+
+This source file can run different code for different python
+ interpreters.
+
+This file has 3 different modes:
+    1. If run as 'python py2and3.py'
+        It will output 'You are using Python 2x'
+    2. If run as 'python3 py2and3.py'
+        It will output 'You are using python 3x'
+    3. If run with any version that doesn't fall under 2x or 3x
+        It will output 'Python version not supported'
+'''
+
 import sys
 from pypreprocessor import pypreprocessor
 
+#exclude
 if sys.version[:3].split('.')[0] == '2':
     pypreprocessor.defines.append('python2')
-    pass
-elif sys.version[:3].split('.')[0] == '3':
+if sys.version[:3].split('.')[0] == '3':
     pypreprocessor.defines.append('python3')
-    pass
-else:
-    print('python' + version.split('.')[0] + ' not supported') 
-    sys.exit(1)
-    
-pypreprocessor.parse()
 
+pypreprocessor.parse()
+#endexclude
 #ifdef python2
 print('You are using Python 2x')
 #ifdef python3
