4a68d9aed3085fd84f2b22d39c02cfd13c9cd139
Evan Plaice <evanplaice@gmail.com>
2010-06-04 14:37:03 -0600

    Added a new example debug2production.py
    debug2production.py
    * added command line arguments to run different modes
    * added debug mode
     * runs the source in on-the-fly mode with 'debug' defined
    * added production mode
     * outputs to a file and removes the metadata
    * added postprocessed mode
     * outputs to a file but preserves the metadata
    * added a docstring describing debug2production.py for the user's benefit
    * cleaned whitespace

---
+++ Examples/debug2production.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# debug2production.py
+
+'''
+debug2production.py - For schitzophrenic source files.
+
+This source file has multiple modes of operation all of which can be
+ controlled by passing an argument on the command line.
+
+This file has 3 different modes:
+    1. debug
+        Run all of the code contained in the #define debug blocks
+         and execute it on the fly.
+        Just run 'python debug2production.py debug'.
+    
+    2. production
+        Output the source to output.py with all of the meta-tags
+         stripped.
+        Just run it type 'python debug2production.py production'
+        
+    3. postprocessed
+        Output the source to output.py but perserve all the
+         metadata.
+        Just run 'python debug2production.py postprocessed'
+'''
+
+import sys
+from pypreprocessor import pypreprocessor
+
+outputFile = 'output_file.py'
+
+#exclude
+# run the script in 'debug' mode
+if 'debug' in sys.argv:
+    pypreprocessor.defines.append('debug')
+
+# run the script in 'production' mode
+if 'production' in sys.argv:
+    pypreprocessor.defines.append('production')
+    pypreprocessor.output = outputFile
+    pypreprocessor.removeMeta = True
+
+# run the script in 'postprocessed' mode
+if 'postprocessed' in sys.argv:
+    pypreprocessor.defines.append('postprocessed')
+    pypreprocessor.output = outputFile
+
+pypreprocessor.parse()
+#endexclude
+#ifdef debug
+print('This script is running in \'debug\' mode')
+#ifdef production
+print('This script is running in \'production\' mode')
+print('To see the output open ' + outputFile)
+#ifdef postprocessed
+print('This script is running in \'postprocessed\' mode')
+print('To see the output open ' + outputFile)
+#endif
