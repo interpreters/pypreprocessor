c350d0dd99a089433779d2e3ececedb9a628ca3d
H_Leusmann <h_leusmann@pc130.iup.uni-heidelberg.de>
2017-04-12 17:52:26 +0200

    Further working on

--- pypreprocessor/__init__.py
+++ pypreprocessor/__init__.py
@@ -9,6 +9,8 @@ __version__ = '0.6.0'
 # changed by hendiol at 18.01.2017: added reset_internal for processing several files after each other
 # changed by hendiol at 11.04.2017: trying to get nested #ifdefs handeld
 
+#develop nested further
+
 import sys
 import os
 import traceback
