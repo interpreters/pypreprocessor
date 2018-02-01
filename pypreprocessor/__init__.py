c07b6267ade0e1010c49963464f01ad9559c4f01
Epikem <dltldls95@naver.com>
2018-02-01 15:35:49 +0900

    make exec run in global

--- pypreprocessor/__init__.py
+++ pypreprocessor/__init__.py
@@ -296,7 +296,7 @@ class preprocessor:
     # postprocessor - on-the-fly execution
     def on_the_fly(self):
         try:
-            exec(open(self.output,"r", encoding=self.readEncoding).read())
+            exec(open(self.output,"r", encoding=self.readEncoding).read(), globals())
         except:
             self.rewrite_traceback()
         finally:
