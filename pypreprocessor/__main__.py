03ce4fff06cea8d9419520f88a08216192638f5f
Laurent Pinson <laurent.pinson@b-com.com>
2021-12-20 10:58:59 +0100

    add __main__ for execution as python -m pypreprocessor ...

---
+++ pypreprocessor/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+from . import preprocessor
+
+parser = argparse.ArgumentParser(
+    description="Check https://github.com/interpreters/pypreprocessor for documentation",
+    epilog="""Examples:
+    python -m """+__package__+""" somepython.py post-proc.py -m -d DEBUG FILE:4 EXEC
+    python -m """+__package__+""" table.json --escape #@ -d NUM:4 ID:1 
+    """,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    usage=' python -m '+__package__+' input [output] [-h] [-r] [-m] [-e ESCAPE] [-d [DEFINE ...]]',
+)
+parser.add_argument("-r", "--run", help="Run on the fly", 
+                    action='store_true', default=False)
+parser.add_argument("-m", "--removeMeta", help="remove meta lines from the output", 
+                    action='store_true', default=False)
+parser.add_argument("-e", "--escape", help="define the escape sequence to use. Default is #")
+parser.add_argument("-d", "--define", help="list of variable to define", nargs='*')
+parser.add_argument("input", help="input file.")
+parser.add_argument("output", nargs='?', help="output file. Default is <input_basename>_out.<input_extension>")
+args = parser.parse_args()
+
+p=preprocessor(inFile=args.input, mode=None, removeMeta=args.removeMeta, escapeChar=None, 
+               run=args.run, resume=False, save=True)
+if args.output:
+    p.define = args.output
+if args.define:
+    for elem in args.define:
+        p.define(*elem.split(':'))
+if args.escape:
+    p.escape = args.escape
+
+p.parse()
